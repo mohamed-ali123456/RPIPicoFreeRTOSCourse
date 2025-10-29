edcd429023d8d0bb089bfec6a35610c0277e743a
mohamed-ali123456 <bouadlamohamedali@gmail.com>
2025-10-29 11:43:08 +0800

    Update main.cpp

--- 4-Tasks/MultiBlink/src/main.cpp
+++ 4-Tasks/MultiBlink/src/main.cpp
@@ -15,16 +15,19 @@
 #include "BlinkAgent.h"
 
 
-
 //Standard Task priority
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD					0
+#define LED_PAD					1
 #define LED1_PAD				2
 #define LED2_PAD				3
 #define LED3_PAD				4
 #define LED4_PAD				5
+#define LED5_PAD				6
+#define LED6_PAD				7
+#define LED7_PAD				8
+#define LED8_PAD				9
 
 
 
@@ -69,6 +72,8 @@ void runTimeStats(   ){
    //Get heap allocation information
    HeapStats_t heapStats;
    vPortGetHeapStats(&heapStats);
+   printf("mohamed-ali bouadla");
+   printf("f14228822");
    printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
 		   heapStats.xAvailableHeapSpaceInBytes,
 		   heapStats.xNumberOfFreeBlocks,
@@ -88,6 +93,10 @@ void mainTask(void *params){
 	BlinkAgent worker2(LED2_PAD);
 	BlinkAgent worker3(LED3_PAD);
 	BlinkAgent worker4(LED4_PAD);
+	BlinkAgent worker5(LED5_PAD);
+	BlinkAgent worker6(LED6_PAD);
+	BlinkAgent worker7(LED7_PAD);
+	BlinkAgent worker8(LED8_PAD);
 
 	printf("Main task started\n");
 
@@ -96,7 +105,11 @@ void mainTask(void *params){
 	worker2.start("Worker 2", TASK_PRIORITY + 1);
 	worker3.start("Worker 3", TASK_PRIORITY + 2);
 	worker4.start("Worker 4", TASK_PRIORITY + 3);
-
+	worker5.start("Worker 5", TASK_PRIORITY + 4);
+	worker6.start("Worker 6", TASK_PRIORITY + 5);
+	worker7.start("Worker 7", TASK_PRIORITY + 6);
+	worker8.start("Worker 8", TASK_PRIORITY + 7);
+	
 	while (true) { // Loop forever
 		runTimeStats();
 		vTaskDelay(3000);
