50f6c113341bf5b82fa876b34b42e9f071785b49
mohamed-ali123456 <bouadlamohamedali@gmail.com>
2025-12-03 11:46:10 +0800

    Update main.cpp

--- 7-MessageBuf/SerialCmds/src/main.cpp
+++ 7-MessageBuf/SerialCmds/src/main.cpp
@@ -24,12 +24,12 @@
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD				0
+#define LED_PAD				25
 #define LED1_PAD			2
 #define LED2_PAD			3
 #define LED3_PAD			4
 #define LED4_PAD			5
-#define LED5_PAD		   15
+#define LED5_PAD		    8
 
 
 void runTimeStats(   ){
@@ -146,9 +146,9 @@ int main( void )
 {
 	//Setup serial over USB and give a few seconds to settle before we start
     stdio_init_all();
-    sleep_ms(2000);
+    sleep_ms(10000);
     printf("GO\n");
-
+	printf("f14228822 bouadlamohamed ali \n f14218807 dridi rayane\n");
     //Start tasks and scheduler
     const char *rtos_name = "FreeRTOS";
     printf("Starting %s on core 0:\n", rtos_name);
