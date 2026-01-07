c1606215498783ffc63e65598d5fc9cc822dc605
mohamed-ali123456 <bouadlamohamedali@gmail.com>
2026-01-07 11:14:10 +0800

    Update main.cpp

--- 10-Final/FreeRTOSSMP/src/main.cpp
+++ 10-Final/FreeRTOSSMP/src/main.cpp
@@ -1,9 +1,6 @@
 /***
- * Demo program to use FreeRTOS SMP on Both Cores
- * Blink on Core 0 to GPIO 0
- * Blink on Core 1 to GPIO 15
- * Counter showing on GPIO 2 to 5, using Core 1
- * Instructions sent to Counter from Main Task on Core 0
+ * Demo program to light 4 LEDs as binary random value
+ * Uses FreeRTOS Task
  * Jon Durrant
  * 15-Aug-2022
  */
@@ -15,28 +12,31 @@
 #include "task.h"
 #include <stdio.h>
 #include <math.h>
-#include "hardware/structs/sio.h"
 
 #include "BlinkAgent.h"
 #include "CounterAgent.h"
+#include "DecoderAgent.h"
 
 
 //Standard Task priority
-#define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
+#define TASK_PRIORITY       ( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD					0
-#define LED1_PAD				2
-#define LED2_PAD				3
-#define LED3_PAD				4
-#define LED4_PAD				5
-#define LED5_PAD			   15
+#define LED_PAD             0
+#define LED1_PAD            2
+#define LED2_PAD            3
+#define LED3_PAD            4
+#define LED4_PAD            5
+#define LED5_PAD            6
+#define LED6_PAD            7
+#define LED7_PAD            8
+#define LED8_PAD            9
 
 
 void runTimeStats(   ){
-	TaskStatus_t *pxTaskStatusArray;
-	volatile UBaseType_t uxArraySize, x;
-	unsigned long ulTotalRunTime;
+    TaskStatus_t *pxTaskStatusArray;
+    volatile UBaseType_t uxArraySize, x;
+    unsigned long ulTotalRunTime;
 
 
    // Get number of takss
@@ -52,34 +52,34 @@ void runTimeStats(   ){
                                  uxArraySize,
                                  &ulTotalRunTime );
 
-	 // Print stats
-	 for( x = 0; x < uxArraySize; x++ )
-	 {
-		 printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
-				pxTaskStatusArray[ x ].xTaskNumber ,
-				pxTaskStatusArray[ x ].uxCurrentPriority ,
-				pxTaskStatusArray[ x ].uxBasePriority ,
-				pxTaskStatusArray[ x ].usStackHighWaterMark ,
-				pxTaskStatusArray[ x ].pcTaskName
-				);
-	 }
+     // Print stats
+     for( x = 0; x < uxArraySize; x++ )
+     {
+         printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
+                pxTaskStatusArray[ x ].xTaskNumber ,
+                pxTaskStatusArray[ x ].uxCurrentPriority ,
+                pxTaskStatusArray[ x ].uxBasePriority ,
+                pxTaskStatusArray[ x ].usStackHighWaterMark ,
+                pxTaskStatusArray[ x ].pcTaskName
+                );
+     }
 
 
       // Free array
       vPortFree( pxTaskStatusArray );
    } else {
-	   printf("Failed to allocate space for stats\n");
+       printf("Failed to allocate space for stats\n");
    }
 
    //Get heap allocation information
    HeapStats_t heapStats;
    vPortGetHeapStats(&heapStats);
    printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
-		   heapStats.xAvailableHeapSpaceInBytes,
-		   heapStats.xNumberOfFreeBlocks,
-		   heapStats.xNumberOfSuccessfulAllocations,
-		   heapStats.xNumberOfSuccessfulFrees
-		   );
+           heapStats.xAvailableHeapSpaceInBytes,
+           heapStats.xNumberOfFreeBlocks,
+           heapStats.xNumberOfSuccessfulAllocations,
+           heapStats.xNumberOfSuccessfulFrees
+           );
 }
 
 
@@ -88,35 +88,27 @@ void runTimeStats(   ){
  * @param params - unused
  */
 void mainTask(void *params){
-	BlinkAgent blink(LED_PAD);
-	BlinkAgent blink1(LED5_PAD);
-	CounterAgent counter(LED1_PAD, LED2_PAD, LED3_PAD, LED4_PAD);
+    char line[80];
+    BlinkAgent blink(LED_PAD);
+    CounterAgent counter(LED1_PAD, LED2_PAD, LED3_PAD, LED4_PAD, LED5_PAD, LED6_PAD, LED7_PAD, LED8_PAD);
+    DecoderAgent decoder(&counter);
 
+    printf("Main task started\n");
 
-	printf("Main task started\n");
+    blink.start("Blink", TASK_PRIORITY);
+    counter.start("Counter", TASK_PRIORITY);
+    decoder.start("Decode", TASK_PRIORITY);
 
-	blink.start("Blink 0", TASK_PRIORITY);
-	blink1.start("Blink 1", TASK_PRIORITY);
-	counter.start("Counter", TASK_PRIORITY);
+    while (true) { // Loop forever
+        runTimeStats();
+        uint16_t r = rand() & 0x0FF;
 
-	//Bind to CORE 1
-	UBaseType_t coreMask = 0x2;
-	vTaskCoreAffinitySet( blink1.getTask(), coreMask );
-	vTaskCoreAffinitySet( counter.getTask(), coreMask );
-
-	//Bind to CORE 0
-	coreMask = 0x1;
-	vTaskCoreAffinitySet( blink.getTask(), coreMask );
-
-
-	while (true) { // Loop forever
-		runTimeStats();
-		printf("Main on Core %ld\n",sio_hw->cpuid);
-
-		uint8_t r = rand()&0xF;
-		counter.on(r);
-		vTaskDelay(3000);
-	}
+        sprintf(line, "{\"count\": %d}\r\n", r);
+        printf("Providing Json %s\n", line);
+        decoder.add(line);
+        vTaskDelay(3000);
+    printf("bouadla mohamed ali, f14228822");
+    }
 }
 
 
@@ -127,7 +119,7 @@ void mainTask(void *params){
  */
 void vLaunch( void) {
 
-	//Start blink task
+    //Start blink task
     TaskHandle_t task;
     xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
 
@@ -141,26 +133,14 @@ void vLaunch( void) {
  */
 int main( void )
 {
-	//Setup serial over USB and give a few seconds to settle before we start
+    //Setup serial over USB and give a few seconds to settle before we start
     stdio_init_all();
     sleep_ms(2000);
     printf("GO\n");
 
     //Start tasks and scheduler
-
-    const char *rtos_name;
-#if ( portSUPPORT_SMP == 1 )
-    rtos_name = "FreeRTOS SMP";
-#else
-    rtos_name = "FreeRTOS";
-#endif
-
-#if ( portSUPPORT_SMP == 1 ) && ( configNUM_CORES == 2 )
-    printf("%s on both cores:\n", rtos_name);
-#else
+    const char *rtos_name = "FreeRTOS";
     printf("Starting %s on core 0:\n", rtos_name);
-#endif
-
     vLaunch();
 
 
